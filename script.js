cc101b873aa84e1c2336ef9ab4c8d72be059af61
Payaswini Gurung <gurung.38@osu.edu>
2026-01-05 22:46:12 +0000

    back

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+document.addEventListener("scroll", () => {
+    const scrollTop = window.scrollY;
+    const docHeight =
+      document.documentElement.scrollHeight - window.innerHeight;
 
+    const darkness = Math.min(scrollTop / docHeight, 1);
+
+    document.body.style.setProperty("--darkness", darkness);
+  });
+  
