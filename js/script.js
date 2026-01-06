f359bd7e35ab15ade880acc5f09af67eaea8ff59
Payaswini Gurung <gurung.38@osu.edu>
2026-01-06 04:51:26 +0000

    com

--- js/script.js
+++ js/script.js
@@ -3,7 +3,10 @@ window.addEventListener("scroll", () => {
   const docHeight =
     document.documentElement.scrollHeight - window.innerHeight;
 
-  const scrollProgress = Math.min(Math.pow(scrollTop / docHeight, 1.3), 1);
+  const scrollProgress = Math.min(
+    Math.pow(scrollTop / docHeight, 1.3),
+    1
+  );
 
   document.documentElement.style.setProperty(
     "--darkness",
